074b7682e3728c9e6a16ccb0ec3d9c92bdabfae3
WENTING SUN <45490316+WentingSun@users.noreply.github.com>
2025-04-12 21:16:00 +0100

    The basic GameWorld and SmallBall Logic is ready

--- Assets/Scripts/SmallBall.cs
+++ Assets/Scripts/SmallBall.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class SmallBall : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
-    [SerializeField] Vector3 velocity;
-    [SerializeField] float velocityMagnitude;
+    [SerializeField] float StayTime;
+    [SerializeField] Transform targetPosition;
 
 
     // Start is called before the first frame update
@@ -18,31 +18,50 @@ public class SmallBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        velocity = rb.velocity;
-        velocityMagnitude = velocity.magnitude;
+        // velocity = rb.velocity;
+        // velocityMagnitude = velocity.magnitude;
     }
 
 
     public void applyForce()
     {
-        rb.AddForce(Vector3.down*3f,ForceMode.Impulse);
+        rb.AddForce(Vector3.down * 3f, ForceMode.Impulse);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("SmallBall Enter");
+        // Debug.Log("SmallBall Enter");
     }
 
     void OnCollisionStay(Collision collision)
     {
-        Debug.Log("CollisionStay");
+        // Debug.Log("CollisionStay");
+        StayTime += Time.deltaTime;
+        if (StayTime > 0.1f)
+        {
+            rb.velocity = Vector3.zero;
+            if(targetPosition != null){
+            rb.AddForce((targetPosition.position-transform.position).normalized * 3, ForceMode.VelocityChange);
+            }else{
+            rb.AddForce((-transform.position).normalized * 3, ForceMode.VelocityChange);
+            }
+            
+        }
+        if (rb.velocity.magnitude < 5f)
+        {
+            Vector3 vector3= rb.velocity;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(vector3.normalized * 2, ForceMode.VelocityChange);
+        }
+
     }
 
     void OnCollisionExit(Collision collision)
     {
-        Debug.Log("SmallBall Exit");
+        StayTime = 0;
+        // Debug.Log("SmallBall Exit");
         Vector3 direction = rb.velocity.normalized;
-        
+
     }
 
 }
