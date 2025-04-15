e1aca5330407d6fd00ef6df56de751994204c1db
wxy404-A <xtcjswxue@163.com>
2025-04-16 06:30:26 +0800

    渲染小球（移动测试版）

--- Assets/Scripts/CameraB.cs
+++ Assets/Scripts/CameraB.cs
@@ -6,13 +6,18 @@ public class CameraB : MonoBehaviour
 {
 
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private Transform bigBall;//获取大球
+    [SerializeField] private float Multiplier = 10f;//倍数距离
 
-    public float scaleRatio = 0.1f;
+    private float BigBallRadius = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (bigBall != null)
+        {
+            BigBallRadius = bigBall.localScale.x / 2f;
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +25,9 @@ public class CameraB : MonoBehaviour
     {
         if (mainCamera != null)
         {
-            transform.position = mainCamera.transform.position * 10;
+            Vector3 direction = (mainCamera.transform.position - Vector3.zero).normalized;//方向
+            float distance = BigBallRadius * Multiplier;//距离
+            transform.position = direction * distance;
             transform.LookAt(Vector3.zero);
         }
     }
